0d80cc053d4851617c14070d19b924d66f74fd79
xxxcskxxx <celalselcukkaraca@gmail.com>
2019-10-03 16:15:21 +0300

    Hesaplama Güncellendi
    Saniye ekleme formata daha uygun olarak güncellendi

--- src/main/java/com/csk/filereader/FileReader_Spiderman/App.java
+++ src/main/java/com/csk/filereader/FileReader_Spiderman/App.java
@@ -16,6 +16,7 @@ public class App
     	
     	//System.out.println(new File("Spider-Man.Far.from.Home.srt").getAbsoluteFile());
 		
+		DecimalFormat decimalFormat = new DecimalFormat("#.000");
 	
     	
     	try(Scanner scan=new Scanner(new FileReader("Spider-Man.Far.from.Home.srt") )){
@@ -41,27 +42,37 @@ public class App
     				
     				String[] hrs_end=parts[1].split(":",3);
     				
-    				double x=Integer.parseInt(hrs[2].substring(0,2))+(Integer.parseInt(hrs[2].substring(3,6)))*(0.001)+a; 				
+    				double x=Integer.parseInt(hrs[2].substring(0,2))+a+(Integer.parseInt(hrs[2].substring(3,6)))*(0.001); 				
     		//		System.out.println("x =" + x);
-    				//System.out.println("x = "+ new DecimalFormat("0.000").format(x-60));
+    		//		System.out.println(decimalFormat.format((x-60)));
+    				
+    				//System.out.println("x =" + new DecimalFormat("##.###").format(x));
+    				// System.out.println(new DecimalFormat("##.###").format(x-60));
+    				//x =Double.parseDouble(new DecimalFormat("##.###").format(x));
+    				//System.out.println("x = "+ new DecimalFormat("##.##").format(x-60));
     				if(x>=60){
-    					
-    					hrs[2]=(Double.toString((x-60))).substring(0,4);
+    				//	hrs[2]="*"+new DecimalFormat("##.###").format(x-60);
+    					hrs[2]=(decimalFormat.format((x-60)));
     					hrs[1]=Integer.toString(Integer.parseInt(hrs[1])+1);
     				}else{
     					
-    					hrs[2]=Double.toString(x);
+    					hrs[2]=decimalFormat.format(x);
     				}
-    				double y=Integer.parseInt(hrs_end[2].substring(0,2))+(Integer.parseInt(hrs_end[2].substring(3,6)))*(0.0001)+a; 				
+    				
+    				
+    		//		double y=Integer.parseInt(hrs_end[2].substring(0,2))+(Integer.parseInt(hrs_end[2].substring(3,6)))*(0.0001)+a; 
+    				double y=Integer.parseInt(hrs_end[2].substring(0,2))+a+(Integer.parseInt(hrs_end[2].substring(3,6)))*(0.001);
     		//		System.out.println("y =" + y);
     				//System.out.println("x = "+ new DecimalFormat("0.000").format(x-60));
     				if(y>=60){
     					
-    					hrs_end[2]=(Double.toString((y-60))).substring(0,4);
+    				//	hrs_end[2]=(Double.toString((y-60))).substring(0,4);
+    					hrs_end[2]=(decimalFormat.format((y-60)));
     					hrs_end[1]=Integer.toString(Integer.parseInt(hrs_end[1])+1);
     				}else{
     					
-    					hrs_end[2]=Double.toString(y);
+    					//hrs_end[2]=Double.toString(y);
+    					hrs_end[2]=decimalFormat.format(y);
     				}
     				
     				
@@ -70,7 +81,7 @@ public class App
     		//		System.out.println("yeni saniye :"+hrs[2]);
     				//Integer.parseInt(hrs[2])
     		//		System.out.println("Eski Saat Aralığı  " +lineinfo);
-    				System.out.println("Yeni Düzenlenmiş saat : "+hrs[0]+":"+hrs[1]+":"+hrs[2].substring(0,4)+" --> "+hrs_end[0]+":"+hrs_end[1]+":"+hrs_end[2].substring(0,4) );
+    				System.out.println("Yeni Düzenlenmiş saat : "+hrs[0]+":"+hrs[1]+":"+hrs[2]+" --> "+hrs_end[0]+":"+hrs_end[1]+":"+hrs_end[2] );
     			
     				
     				
